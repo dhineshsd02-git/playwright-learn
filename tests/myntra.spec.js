9386e35728377c3a4b34cb47bc2fd4d1f89a9f1d
dhineshsd02-git <dhineshsd1998@gmail.com>
2026-07-03 22:10:58 +0530

    changed myntra

--- tests/myntra.spec.js
+++ tests/myntra.spec.js
@@ -22,15 +22,15 @@ await page.locator('[alt$="ENC (Indigo Blue)"]').click();
 await page.waitForTimeout(4000);
 })
 
-// test('locator',async({page})=>{
-// await page.goto('https://www.amazon.in/');
-// //await page.locator('#nav-link-accountList-nav-line-1').click();
-// await page.locator('[name="field-keywords"]').fill('headphones');
-// await page.locator('#nav-search-submit-button').click();
-// await page.waitForTimeout(2000);
-// await page.locator('[alt*="ENC (Indigo Blue)"]').click();
-// await page.waitForTimeout(4000);
-// })
+test('locator',async({page})=>{
+await page.goto('https://www.amazon.in/');
+//await page.locator('#nav-link-accountList-nav-line-1').click();
+await page.locator('[name="field-keywords"]').fill('headphones');
+await page.locator('#nav-search-submit-button').click();
+await page.waitForTimeout(2000);
+await page.locator('[alt*="ENC (Indigo Blue)"]').click();
+await page.waitForTimeout(4000);
+})
 
 
 
