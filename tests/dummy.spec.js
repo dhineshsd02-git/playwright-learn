eff21d63156ef46d9d80fe1b2d04c08b62f8cb3a
dhineshsd02-git <dhineshsd1998@gmail.com>
2026-07-06 17:58:58 +0530

    merge

--- tests/dummy.spec.js
+++ tests/dummy.spec.js
@@ -3,4 +3,8 @@ await page.goto('https://www.amazon.in/');
 //await page.locator('#nav-link-accountList-nav-line-1').click();
 await page.locator('[name="field-keywords"]').fill('headphones');
 await page.locator('#nav-search-submit-button').click();
+await page.waitfortimeout(3000);
+await page.waitfortimeout(3000);
+
+
 })
