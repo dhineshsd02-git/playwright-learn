0ab42bbe399aff24d11c64366cacdea9ab04e482
dhineshsd02-git <dhineshsd1998@gmail.com>
2026-07-07 16:51:48 +0530

    checkmerge

--- tests/dummy.spec.js
+++ tests/dummy.spec.js
@@ -3,4 +3,6 @@ await page.goto('https://www.amazon.in/');
 //await page.locator('#nav-link-accountList-nav-line-1').click();
 await page.locator('[name="field-keywords"]').fill('headphones');
 await page.locator('#nav-search-submit-button').click();
+await page.click();
+
 })
